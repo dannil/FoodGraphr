473d32137314b3979bd0a9a116f0a6808a76c79a
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-10 15:11:35 +0100

    Added controller for start page

---
+++ FoodGraphr/Controller/IndexController.cs
@@ -0,0 +1,32 @@
+﻿using FoodGraphr.Model;
+using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodGraphr.Controller
+{
+    public class IndexController : NancyModule
+    {
+        private API api;
+
+        public IndexController()
+        {
+            Get["/"] = _ =>
+            {
+                return View["index"];
+            };
+
+            Post["/"] = _ =>
+            {
+                api = new API();
+
+                int id = Request.Form["id"];
+
+                Food f = api.GetFood(id);
+                return View["food", f];
+            };
+        }
+    }
+}
