57842eca39c4f3392b72120c893afb387cf645e7
icedoor <isadorapersson@hotmail.com>
2015-03-17 14:34:12 +0100

    generate chart from food, display in html

--- FoodGraphr/Controller/FoodController.cs
+++ FoodGraphr/Controller/FoodController.cs
@@ -40,32 +40,9 @@ namespace FoodGraphr.Controller
 
                 System.Diagnostics.Debug.WriteLine("TOTAL: " + total);
 
-                PieChart chart = new PieChart(540, 280);
+                f.GeneratePieChart();
 
-                chart.SetTitle(f.Name);
-                chart.SetData(new float[] { f.NutrientValues["fat"],
-                                            f.NutrientValues["protein"],
-                                            f.NutrientValues["carbohydrates"],
-                                            f.NutrientValues["fibres"],                                             
-                                            f.NutrientValues["salt"],
-                                            f.NutrientValues["ash"],
-                                            f.NutrientValues["water"],
-                                            f.NutrientValues["alcohol"]});
-
-                chart.SetLabels(new string[] { f.NutrientValues["fat"].ToString() + " g",
-                                               f.NutrientValues["protein"].ToString() + " g",
-                                               f.NutrientValues["carbohydrates"].ToString() + " g",
-                                               f.NutrientValues["fibres"].ToString() + " g",
-                                               f.NutrientValues["salt"].ToString() + " g",
-                                               f.NutrientValues["ash"].ToString() + " g",
-                                               f.NutrientValues["water"].ToString() + " g",
-                                               f.NutrientValues["alcohol"].ToString() + " g"});
-
-                chart.SetLegends(new string[] { "Fat", "Protein", "Carbohydrates", "Fibres", "salt", "ash", "water", "alcohol" });
-
-                chart.SetColors(new string[] { "EBC30C", "D42000", "29E14E", "DDDDDD", "473E3F", "0F1C2E", "4D8DEB", "2D5187" });
-
-                System.Diagnostics.Debug.WriteLine(chart.GetUrl());
+                System.Diagnostics.Debug.WriteLine(f.Chart.GetUrl());
 
                 return View["food", f];
             };            
--- FoodGraphr/Model/Food.cs
+++ FoodGraphr/Model/Food.cs
@@ -7,6 +7,7 @@ namespace FoodGraphr.Model
     {
         private int number;
         private string name;
+        private Charts.Chart myChart;
         private Dictionary<String, float> nutrientValues;
 
         public string Name
@@ -21,11 +22,44 @@ namespace FoodGraphr.Model
             set { number = value; }
         }
 
+        public Charts.Chart Chart
+        {
+            get { return myChart; }
+            set { myChart = value; }
+        }
+
         public Dictionary<string, float> NutrientValues
         {
             get { return nutrientValues; }
             set { nutrientValues = value; }
         }
 
+        public void GeneratePieChart()
+        {
+            myChart = new Model.Charts.PieChart(540, 280);
+
+            myChart.SetTitle(name);
+            myChart.SetData(new float[] { nutrientValues["fat"],
+                                          nutrientValues["protein"],
+                                          nutrientValues["carbohydrates"],
+                                          nutrientValues["fibres"],                                             
+                                          nutrientValues["salt"],
+                                          nutrientValues["ash"],
+                                          nutrientValues["water"],
+                                          nutrientValues["alcohol"]});
+
+            myChart.SetLabels(new string[] { nutrientValues["fat"].ToString() + " g",
+                                             nutrientValues["protein"].ToString() + " g",
+                                             nutrientValues["carbohydrates"].ToString() + " g",
+                                             nutrientValues["fibres"].ToString() + " g",
+                                             nutrientValues["salt"].ToString() + " g",
+                                             nutrientValues["ash"].ToString() + " g",
+                                             nutrientValues["water"].ToString() + " g",
+                                             nutrientValues["alcohol"].ToString() + " g"});
+
+            myChart.SetLegends(new string[] { "Fat", "Protein", "Carbohydrates", "Fibres", "salt", "ash", "water", "alcohol" });
+
+            myChart.SetColors(new string[] { "EBC30C", "D42000", "29E14E", "DDDDDD", "473E3F", "0F1C2E", "4D8DEB", "2D5187" });
+        }
     }
 }
