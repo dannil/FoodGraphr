14c76da8a5831ad26b57565305d0dd501fc36bfc
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-19 12:45:10 +0100

    Removed debugging output

--- FoodGraphr/Controller/FoodController.cs
+++ FoodGraphr/Controller/FoodController.cs
@@ -23,32 +23,6 @@ namespace FoodGraphr.Controller
                 {
                     return View["Error/404"];
                 }
-
-                double total = 0;
-                foreach (KeyValuePair<string, float> pair in f.NutrientValues)
-                {
-                    System.Diagnostics.Debug.WriteLine(pair.Key + " " + pair.Value + " " + test[pair.Key].Unit);
-
-                    switch (test[pair.Key].Unit)
-                    {
-                        case "g":
-                            total += pair.Value;
-                            break;
-                        case "mg" :
-                            total += pair.Value / 1000;
-                            break;
-                        case "ug" :
-                             total += pair.Value / 1000000;
-                             break;                        
-                    }                        
-                }
-
-                System.Diagnostics.Debug.WriteLine("TOTAL: " + total);
-
-                f.GeneratePieChart();
-
-                System.Diagnostics.Debug.WriteLine(f.ChartUrl);
-
                 return View["food", f];
             };            
         }
