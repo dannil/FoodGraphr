c88dc852b3bf6d00504bbe051285c1edc46c1a89
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-19 12:36:03 +0100

    Fixed NPE on empty search

--- FoodGraphr/Controller/IndexController.cs
+++ FoodGraphr/Controller/IndexController.cs
@@ -25,11 +25,21 @@ namespace FoodGraphr.Controller
                 string name = Request.Form["name"];
 
                 List<Food> foods = api.SearchFood(name);
-                List<Food> sortedFoods = foods.OrderByDescending(f => f.Name.IndexOf(name.First().ToString().ToUpper() + name.Substring(1)))
-                                              .ThenByDescending(f => f.Name.IndexOf(name.First().ToString().ToUpper() + name.Substring(1) + " "))
-                                              .ThenBy(f => f.Name.Split(' ').Count())
-                                              .ToList();
 
+                List<Food> sortedFoods = new List<Food>();
+                if (name != null)
+                {
+                    // Sort the list by the input
+                    sortedFoods = foods.OrderByDescending(f => f.Name.IndexOf(name.First().ToString().ToUpper() + name.Substring(1)))
+                                                  .ThenByDescending(f => f.Name.IndexOf(name.First().ToString().ToUpper() + name.Substring(1) + " "))
+                                                  .ThenBy(f => f.Name.Split(' ').Count())
+                                                  .ToList();
+                }
+                else
+                {
+                    // The user didn't search for anything, order by default (which is Number)
+                    sortedFoods = foods;
+                }
                 return View["searchresult", sortedFoods];
             };
         }
