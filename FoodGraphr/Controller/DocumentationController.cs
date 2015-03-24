c4605b70543968729d82a8639430f6673b040b30
icedoor <isadorapersson@hotmail.com>
2015-03-24 12:49:56 +0100

    added docuemntation page

---
+++ FoodGraphr/Controller/DocumentationController.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using FoodGraphr.Model;
+using FoodGraphr.Model.Charts;
+using System.Collections.Generic;
+using System.Web;
+using FoodGraphr.Model.API;
+
+namespace FoodGraphr.Controller
+{
+    public class DocumentationController: NancyModule
+    {
+        private MatAPI api;
+        public DocumentationController() : base("/documentation")
+        {
+            api = new MatAPI();
+
+            Get["/"] = _ =>
+            {
+                return View["documentation"];
+            }; 
+        }
+    }
+}
