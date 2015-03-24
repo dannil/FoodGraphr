8e5f0f1ef9e1e8ede2902bd4ea89cf89caef7dfe
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-24 14:46:49 +0100

    Fixed custom favicon on API

--- FoodGraphr/Bootstrapper.cs
+++ FoodGraphr/Bootstrapper.cs
@@ -5,9 +5,22 @@ namespace FoodGraphr
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        private byte[] favicon;
+
         protected override byte[] FavIcon
         {
-            get { return null; }
+            get { return this.favicon ?? (this.favicon = LoadFavIcon()); }
+        }
+
+        private byte[] LoadFavIcon()
+        {
+            //TODO: remember to replace 'AssemblyName' with the prefix of the resource
+            using (var resourceStream = GetType().Assembly.GetManifestResourceStream("FoodGraphr.Content.Images.icon.ico"))
+            {
+                var tempFavicon = new byte[resourceStream.Length];
+                resourceStream.Read(tempFavicon, 0, (int)resourceStream.Length);
+                return tempFavicon;
+            }
         }
     }
 }
