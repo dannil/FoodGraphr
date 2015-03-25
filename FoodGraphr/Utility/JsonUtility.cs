ec20aa66b2fe2f2e6886d3144ba0ed4c5cd3b392
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-03-25 07:56:35 +0100

    Added comments

--- FoodGraphr/Utility/JsonUtility.cs
+++ FoodGraphr/Utility/JsonUtility.cs
@@ -9,28 +9,47 @@ using System.Web;
 
 namespace FoodGraphr.Utility
 {
+    /// <summary>
+    /// Class for JSON functionality on objects
+    /// </summary>
     public class JsonUtility
     {
+        /// <summary>
+        /// Convert an object to JSON
+        /// </summary>
+        /// <param name="o">The object to be converted.</param>
+        /// <returns>The JSON representation of the converted object.</returns>
         public static string ConvertToJson(object o)
         {
             JsonSerializerSettings settings = new JsonSerializerSettings
             {
+                // Convert the JSON to camelCase
                 ContractResolver = new CamelCasePropertyNamesContractResolver(),
+
+                // Ignore null values on conversion
                 NullValueHandling = NullValueHandling.Ignore
             };
 
             return JsonConvert.SerializeObject(o, settings);
         }
 
+        /// <summary>
+        /// Sends a JsonResponse through the web framework; is utilized
+        /// to display error messages in JSON format
+        /// </summary>
+        /// <param name="path">The path the user was on when the response happened.</param>
+        /// <param name="statusCode">The current status code of the request.</param>
+        /// <param name="message">The message to be displayed to the user.</param>
+        /// <returns></returns>
         public static JsonResponse DoResponse(string path, HttpStatusCode statusCode, string message) {
-            Nancy.Responses.DefaultJsonSerializer _defaultSerializer = new Nancy.Responses.DefaultJsonSerializer(); 
+            DefaultJsonSerializer defaultSerializer = new DefaultJsonSerializer(); 
             
             return new JsonResponse(new
                                    {
                                        path = path,
                                        code = (int)statusCode,
                                        message = message
-                                   }, _defaultSerializer) { StatusCode = statusCode };    
+                                   }, defaultSerializer) { StatusCode = statusCode };    
         }
     }
 }
